442105dda0ae50400154a061bd20c15c60611dbc
Sourabh Jaishwal <jaishwalsourabh2000@gmail.com>
2024-02-24 15:59:39 +0530

    switch case

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,35 @@
+// switch case statement
+
+// syntax
+/*
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+*/
+
+const month = "March"
+
+switch (month) {
+    case "Jan":
+        console.log("January")
+        break;
+    case "Feb":
+        console.log("February")
+        break;
+    case "March":
+        console.log("March")
+        break;
+    case "April":
+        console.log("April")
+
+    default:
+        console.log("default case match")
+        break;
+}
+
+// cases can be written in numbers and strings
